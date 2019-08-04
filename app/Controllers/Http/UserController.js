fee53a3887be20b40996030819b4b1bc2dd506ab
Matt Burgess <matt@smallbatch.io>
2019-08-05 03:55:49 +0700

    Fix user controller to allow user post

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -9,19 +9,6 @@ const User = use('App/Models/User');
  * Resourceful controller for interacting with users
  */
 class UserController {
-  /**
-   * Show a list of all users.
-   * GET users
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
-    const users = await User.all();
-    return {users};
-  }
 
   /**
    * Create/save a new user.
@@ -31,50 +18,11 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
-    return await User.create(request.user);
+  async store ({ request }) {
+    const formData = JSON.parse(request.raw());
+    return await User.create(formData.user);
   }
 
-  /**
-   * Display a single user.
-   * GET users/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
-    const user = await User.findOrFail(params.id);
-    return {user};
-  }
-
-  /**
-   * Update user details.
-   * PUT or PATCH users/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-    const user = await User.findOrFail(params.id);
-    user.fill(request.user);
-    await user.save();
-  }
-
-  /**
-   * Delete a user with id.
-   * DELETE users/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
-    const user = await User.findOrFail(params.id);
-    user.delete();
-  }
 }
 
 module.exports = UserController
--- start/routes.js
+++ start/routes.js
@@ -18,12 +18,13 @@ const Route = use('Route')
 
 Route.group(() => {
   Route.post('api/auth/attempt', 'AuthController.logIn');
+  Route.post('api/users', 'UserController.store');
 });
 
 Route.group(() => {
   Route.get('varieties', 'VarietyController.index');
   Route.get('address', 'MappingController.address');
-  Route.resource('users', 'UserController').apiOnly();
+  //Route.resource('users', 'UserController').apiOnly();
   Route.resource('containers', 'ContainerController').apiOnly();
   Route.resource('dockets', 'DocketController').apiOnly();
   Route.resource('robbings', 'RobbingController').apiOnly();
